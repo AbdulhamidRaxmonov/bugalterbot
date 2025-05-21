9b38ed1feade8467e169c5eedb30c0180b4b4e3f
Abdulhamid Raxmonov <94451795+AbdulhamidRaxmonov@users.noreply.github.com>
2025-05-21 07:24:13 -0700

    Update bot.php
    dasdsad

--- bot.php
+++ bot.php
@@ -2,13 +2,11 @@
 
 include 'Telegram.php';
 
-
 $telegram = new Telegram('7951838833:AAEIAn5N3cSy33mdsreMC-d80piCSb-Upfw');
 
 $chat_id = $telegram->ChatID();
 $content = array('chat_id' => $chat_id, 'text' => 'Test');
 $telegram->sendMessage($content);
-
 
 
 ?>
