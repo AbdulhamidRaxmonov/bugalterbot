ee328f7ac8c21a06cd177e87765052c1352198f5
Abdulhamid Raxmonov <94451795+AbdulhamidRaxmonov@users.noreply.github.com>
2025-05-21 06:56:50 -0700

    Update bot.php
    dsasd

--- bot.php
+++ bot.php
@@ -1,43 +1,14 @@
-<?
+<?php
 
 include 'Telegram.php';
 
+
 $telegram = new Telegram('7951838833:AAEIAn5N3cSy33mdsreMC-d80piCSb-Upfw');
 
-$data = $telegram->getData();
-$message = $data['message'];
-$text = $message['text'];
-$chat_id = $message['chat']['id'];
-
-if ($text == "/start") {
-     $telegram->sendMessage([
-        'chat_id' => $chat_id,
-        'text' => $text
-    ]);
-}
-
-function chooseLanguage()
-{
-    global $telegram, $chat_id;
-
-    $text = "Пожалуйста выберите язык.\nIltimos, tilni tanlang.";
-
-    setPage($chat_id, 'start');
-
-    $option = array(
-        array($telegram->buildKeyboardButton("Русский 🇷🇺"), $telegram->buildKeyboardButton("O'zbek tili 🇺🇿")),
-    );
-    $keyb = $telegram->buildKeyBoard($option, $onetime = false, $resize = true);
-    $content = array('chat_id' => $chat_id, 'text' => $text, 'reply_markup' => $keyb);
-    $telegram->sendMessage($content);
-}
+$chat_id = $telegram->ChatID();
+$content = array('chat_id' => $chat_id, 'text' => 'Test');
+$telegram->sendMessage($content);
 
 
-function sendMessage($text)
-{
-    global $telegram, $chat_id;
-    $telegram->sendMessage([
-        'chat_id' => $chat_id,
-        'text' => $text
-    ]);
-}
+
+?>
